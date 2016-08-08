569e9e69bd6a032edc9ededc8b05cb457471ec67
Harshad Nawathe <harshad.nawathe@gmail.com>
2016-08-08 08:01:20 +0000

    Adds Stable Partition with additional array

--- dsa/partition/stablePartition.go
+++ dsa/partition/stablePartition.go
@@ -4,7 +4,7 @@ import "github.com/harshadnawathe/go-lang-fun/dsa/rotation"
 
 type Predicate func(int) bool
 
-func StablePartition(a []int, pred Predicate) int {
+func StablePartitionRecursive(a []int, pred Predicate) int {
 	length := len(a)
 
 	if length == 0 {
@@ -24,4 +24,29 @@ func StablePartition(a []int, pred Predicate) int {
 	n2 := StablePartition(a[mid:], pred) + mid
 
 	return n1 + rotation.Rotate(a[n1:n2], mid-n1)
+}
+
+func StablePartitionWithAuxiliaryArray(a []int, pred Predicate) int {
+	aa := make([]int, len(a))
+	good, bad := -1, -1
+	for i := range a {
+		if pred(a[i]) {
+			good++
+			a[good] = a[i]
+		} else {
+			bad++
+			aa[bad] = a[i]
+		}
+	}
+
+	for _, b := range aa[0 : bad+1] {
+		good++
+		a[good] = b
+	}
+
+	return len(a) - bad - 1
+}
+
+func StablePartition(a []int, pred Predicate) int {
+	return StablePartitionWithAuxiliaryArray(a, pred)
 }
